291852e8abd4e819c3c075375b320a61bb7f4e32
Ritik Srivastava <ritik.space@gmail.com>
2019-04-18 16:59:53 +0530

    Admin Panel Interaction

--- public/assets/admin/adminscript.js
+++ public/assets/admin/adminscript.js
@@ -1,5 +1,8 @@
 /* global $ */
 
+var margin = (($(window).width() % $('.element').width()) / 2);
+$('.container').css('margin-left', margin + 20 + 'px');
+$('.container').css('margin-right', margin - 20 + 'px');
 $('.btn').click(function() {
   $('.circle_loader').addClass('animation');
   $('.btn').addClass('animation_circle');
@@ -11,6 +14,8 @@ var del = function() {
     $(".btn").css("animation", "popout-btn 0.3s both ease-in-out 0.5s");
   }, 100);
   setTimeout(function() {
+    $('.container').css('margin-left', margin + 'px');
+    $('.container').css('margin-right', margin + 'px');
     $(".photo").css("animation", "rotate-photo-reverse 0.5s forwards ease-in-out");
     $(".profile").css("animation", "hide-profile 0.5s forwards ease-in-out");
   }, 800);
