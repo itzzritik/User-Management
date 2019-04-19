bd889a3fd545b99460dee8931a4710e4102b84b6
Ritik Srivastava <ritik.space@gmail.com>
2019-04-20 00:44:53 +0530

    Sweet Alerts 2.0

--- public/assets/admin/adminscript.js
+++ public/assets/admin/adminscript.js
@@ -1,68 +1,80 @@
 /* global $ */
-/* global swal */
-
+/* global Swal */
 var itemsPerRow, itemsPerColumn;
 
-$(window).resize(function() { layout(); });
+$(window).resize(function() {
+	layout();
+});
 
 function layout() {
-  var zoomlvl = $(window).width() / 430;
-  itemsPerRow = parseInt($(window).width() / (430), 10);
-  itemsPerColumn = parseInt(($(window).height() + 50) / (140), 10);
+	var zoomlvl = $(window).width() / 430;
+	itemsPerRow = parseInt($(window).width() / (430), 10);
+	itemsPerColumn = parseInt(($(window).height() + 50) / (140), 10);
 
-  if ($(window).width() < 430) {
-    itemsPerRow = 1;
-    itemsPerRow = parseInt($(window).width() / (430 * zoomlvl), 10);
-    itemsPerColumn = parseInt(($(window).height() + 50) / (140 * zoomlvl), 10);
-    $('body').css({ zoom: zoomlvl, '-moz-transform': 'scale(' + zoomlvl + ')' });
-    $('.container').css('margin-left', +20 + 'px');
-    $('.container').css('margin-right', -20 + 'px');
-  }
-  else {
-    var margin = (($(window).width() % 430) / 2);
+	if ($(window).width() < 430) {
+		itemsPerRow = 1;
+		itemsPerRow = parseInt($(window).width() / (430 * zoomlvl), 10);
+		itemsPerColumn = parseInt(($(window).height() + 50) / (140 * zoomlvl), 10);
+		$('body').css({
+			zoom: zoomlvl,
+			'-moz-transform': 'scale(' + zoomlvl + ')'
+		});
+		$('.container').css('margin-left', +20 + 'px');
+		$('.container').css('margin-right', -20 + 'px');
+	}
+	else {
+		var margin = (($(window).width() % 430) / 2);
 
-    $('body').css({ zoom: 1, '-moz-transform': 'scale(1)' });
-    $('.container').css('margin-left', margin + 20 + 'px');
-    $('.container').css('margin-right', margin - 20 + 'px');
-  }
+		$('body').css({
+			zoom: 1,
+			'-moz-transform': 'scale(1)'
+		});
+		$('.container').css('margin-left', margin + 20 + 'px');
+		$('.container').css('margin-right', margin - 20 + 'px');
+	}
 }
 layout();
 
 $('.container').on('click', '.btn', function() {
-  swal({
-      title: "Are You Sure?",
-      text: "Deleted Accounts Cannot Be Recovered!",
-      icon: "warning",
-      buttons: true,
-      dangerMode: true,
-    })
-    .then((willDelete) => {
-      if (willDelete) {
-        var card = $(this).parent().parent();
-        card.find('.circle_loader').addClass('animation');
-        card.find('.btn').addClass('animation_circle');
-        card.find('.profile').addClass('animation_card');
-        console.log(card.find('#emailId').text() + " Deleted Successfully");
-        del(card);
-      }
-    });
+	Swal.fire({
+		title: 'Are you sure?',
+		text: "Once Deleted,You Won't Be Able To Revert This!",
+		type: 'warning',
+		showCancelButton: true,
+		confirmButtonColor: '#3085d6',
+		cancelButtonColor: '#d33',
+		confirmButtonText: 'Yes, delete it!'
+	}).then((result) => {
+		if (result.value) {
+			var card = $(this).parent().parent();
+			card.find('.circle_loader').addClass('animation');
+			card.find('.btn').addClass('animation_circle');
+			card.find('.profile').addClass('animation_card');
+			console.log(card.find('#emailId').text() + " Deleted Successfully");
+			del(card);
+		}
+	});
 });
 var del = function(card) {
-  setTimeout(function() {
-    card.find(".btn").css("animation", "popout-btn 0.3s both ease-in-out 0.5s");
-  }, 100);
-  setTimeout(function() {
-    card.find(".photo").css("animation", "rotate-photo-reverse 0.5s forwards ease-in-out");
-    card.css("animation", "hide-profile 0.5s forwards ease-in-out");
-    card.css("margin-left", "-20px");
-    card.css("transition-duration", "0.5s");
-    setTimeout(function() {
-      card.find(".content").remove();
-      card.find(".photo").css("animation", "popout-btn 0.3s both ease-in-out 0.5s");
-    }, 460);
-    setTimeout(function() { card.parent().css("animation", "hide-card 0.3s both ease-in-out 0.5s"); }, 800);
-    setTimeout(function() { card.parent().remove(); }, 2000);
-  }, 1000);
+	setTimeout(function() {
+		card.find(".btn").css("animation", "popout-btn 0.3s both ease-in-out 0.5s");
+	}, 100);
+	setTimeout(function() {
+		card.find(".photo").css("animation", "rotate-photo-reverse 0.5s forwards ease-in-out");
+		card.css("animation", "hide-profile 0.5s forwards ease-in-out");
+		card.css("margin-left", "-20px");
+		card.css("transition-duration", "0.5s");
+		setTimeout(function() {
+			card.find(".content").remove();
+			card.find(".photo").css("animation", "popout-btn 0.3s both ease-in-out 0.5s");
+		}, 460);
+		setTimeout(function() {
+			card.parent().css("animation", "hide-card 0.3s both ease-in-out 0.5s");
+		}, 800);
+		setTimeout(function() {
+			card.parent().remove();
+		}, 2000);
+	}, 1000);
 };
 
 
@@ -70,42 +82,44 @@ const http = new XMLHttpRequest();
 http.open('POST', '/admin');
 http.setRequestHeader('Content-type', 'application/json');
 http.onload = function() {
-  const data = JSON.parse(http.responseText);
-  console.log(data);
+	const data = JSON.parse(http.responseText);
+	console.log(data);
 
-  function addCards(i, delay) {
-    setTimeout(function() {
-      var element =
-        '<div class="element">' +
-        '<div class="profile">' +
-        '<div class="photo"><img src="https://avatars.dicebear.com/v2/avataaars/' + data[i].emailId + '.svg" /></div>' +
-        '<div class="content">' +
-        '<div class="text">' +
-        '<h3>' + data[i].userName + '</h3>' +
-        '<h6 id="emailId">' + data[i].emailId + '</h6>' +
-        '<h6>' + data[i].phoneNo + '</h6>' +
-        '</div>' +
-        '<div class="btn">' +
-        '<div class="circle_loader"></div>' +
-        '<img src="/public/img/del.svg" />' +
-        '</div></div></div></div>';
-      $('.container').append(element);
-      if (++i < data.length) {
-        if (i < itemsPerColumn * itemsPerRow) {
-          addCards(i, 100);
-        }
-        else if (i == itemsPerColumn * itemsPerRow) {
-          addCards(i, 500);
-        }
-        if (i > itemsPerColumn * itemsPerRow) {
-          addCards(i, 0);
-          if (i == data.length - 1) {
-            setTimeout(function() { $('body, html').css("overflow-y", "auto"); }, 500);
-          }
-        }
-      }
-    }, delay);
-  }
-  addCards(0, 500);
+	function addCards(i, delay) {
+		setTimeout(function() {
+			var element =
+				'<div class="element">' +
+				'<div class="profile">' +
+				'<div class="photo"><img src="https://avatars.dicebear.com/v2/avataaars/' + data[i].emailId + '.svg" /></div>' +
+				'<div class="content">' +
+				'<div class="text">' +
+				'<h3>' + data[i].userName + '</h3>' +
+				'<h6 id="emailId">' + data[i].emailId + '</h6>' +
+				'<h6>' + data[i].phoneNo + '</h6>' +
+				'</div>' +
+				'<div class="btn">' +
+				'<div class="circle_loader"></div>' +
+				'<img src="/public/img/del.svg" />' +
+				'</div></div></div></div>';
+			$('.container').append(element);
+			if (++i < data.length) {
+				if (i < itemsPerColumn * itemsPerRow) {
+					addCards(i, 100);
+				}
+				else if (i == itemsPerColumn * itemsPerRow) {
+					addCards(i, 500);
+				}
+				if (i > itemsPerColumn * itemsPerRow) {
+					addCards(i, 0);
+					if (i == data.length - 1) {
+						setTimeout(function() {
+							$('body, html').css("overflow-y", "auto");
+						}, 500);
+					}
+				}
+			}
+		}, delay);
+	}
+	addCards(0, 500);
 };
 http.send();
--- public/assets/login/loginscript.js
+++ public/assets/login/loginscript.js
@@ -1,165 +1,183 @@
 /* global $ */
-/* global swal */
+/* global Swal */
 
 $('.form-control').focusout(function() {
-    $('.form-group').removeClass('focus');
+	$('.form-group').removeClass('focus');
 });
 $('.form-control').focus(function() {
-    $(this).closest('.form-group').addClass('focus');
+	$(this).closest('.form-group').addClass('focus');
 });
 
 $('.form-control').keyup(function() {
-    if ($(this).val().length > 0) {
-        $(this).closest('.form-group').addClass('filled');
-    }
-    else {
-        $(this).closest('.form-group').removeClass('filled');
-    }
+	if ($(this).val().length > 0) {
+		$(this).closest('.form-group').addClass('filled');
+	}
+	else {
+		$(this).closest('.form-group').removeClass('filled');
+	}
 });
 
 var $formControl = $('.form-control');
-var values = {};
-var validate = $formControl.each(function() {
-    if ($(this).val().length > 0) {
-        $(this).closest('.form-group').addClass('filled');
-    }
-    else {
-        $(this).closest('.form-group').removeClass('filled');
-    }
+$formControl.each(function() {
+	if ($(this).val().length > 0) {
+		$(this).closest('.form-group').addClass('filled');
+	}
+	else {
+		$(this).closest('.form-group').removeClass('filled');
+	}
 });
 
 $('.log').click(function() {
-    $('.center').css('animation', 'logoutanim 0.5s forwards cubic-bezier(0.86, 0, 0.07, 1)');
-    setTimeout(function() { $('.frame').css('animation', 'logoutanim 0.5s forwards cubic-bezier(0.86, 0, 0.07, 1)'); }, 200);
-    $(this).remove();
-    setTimeout(function() { window.location.replace("/login"); }, 500);
+	$('.center').css('animation', 'logoutanim 0.5s forwards cubic-bezier(0.86, 0, 0.07, 1)');
+	setTimeout(function() { $('.frame').css('animation', 'logoutanim 0.5s forwards cubic-bezier(0.86, 0, 0.07, 1)'); }, 200);
+	$(this).remove();
+	setTimeout(function() { window.location.replace("/login"); }, 500);
 });
 
 $('.close').click(function() {
-    $(this).closest('.register-form').toggleClass('open');
+	$(this).closest('.register-form').toggleClass('open');
 });
 
 $('.formset .btn').click(function() {
-    var id = $('.id').val(),
-        pass = $('.pass').val();
-    if (id != "" && pass != "") {
-        const http = new XMLHttpRequest();
-        http.open('POST', '/login');
-        http.setRequestHeader('Content-type', 'application/json');
-        http.onreadystatechange = function() {
-            if (http.readyState == XMLHttpRequest.DONE) {
-                if (http.responseText == 1) {
-                    swal("Congratulations!", ", You are succesfully logged in!", "success")
-                        .then(() => {
-                            var url = '/profile';
-                            var form = $('<form action="' + url + '" method="post">' +
-                                '<input type="hidden" name="email" value="' + id + '" />' +
-                                '<input type="hidden" name="pass" value="' + pass + '" />' +
-                                '</form>');
-                            $('body').append(form);
-                            form.submit();
-                        });
-                }
-                else if (http.responseText == 0) {
-                    swal("Alert!", ", Incorrect password!", "error");
-                }
-                else if (http.responseText == 2) {
-                    swal("Please Check Email!", ",This Account Doesn't Exists!", "error");
-                }
-            }
-        };
-        http.send(JSON.stringify({
-            email: id,
-            pass: pass
-        }));
-    }
+	var id = $('.id').val(),
+		pass = $('.pass').val();
+	if (id != "" && pass != "") {
+		const http = new XMLHttpRequest();
+		http.open('POST', '/login');
+		http.setRequestHeader('Content-type', 'application/json');
+		http.onreadystatechange = function() {
+			if (http.readyState == XMLHttpRequest.DONE) {
+				if (http.responseText == 1) {
+					Swal.fire({
+						type: 'success',
+						title: 'Congratulations!',
+						text: "You are succesfully logged in!",
+						timer: 1500,
+						onBeforeOpen: () => Swal.showLoading()
+					}).then(() => {
+						var url = '/profile';
+						var form = $('<form action="' + url + '" method="post">' +
+							'<input type="hidden" name="email" value="' + id + '" />' +
+							'<input type="hidden" name="pass" value="' + pass + '" />' +
+							'</form>');
+						$('body').append(form);
+						form.submit();
+					});
+				}
+				else if (http.responseText == 0) {
+					Swal.fire({
+						type: 'error',
+						title: 'Alert!',
+						text: 'Incorrect password!'
+					});
+				}
+				else if (http.responseText == 2) {
+					Swal.fire({
+						type: 'error',
+						title: 'Please Check Email!',
+						text: "This Account Doesn't Exists!"
+					});
+				}
+			}
+		};
+		http.send(JSON.stringify({
+			email: id,
+			pass: pass
+		}));
+	}
 });
 
 
 $(".b").on("keyup", function(e) {
-    e.target.value = e.target.value.toLowerCase();
+	e.target.value = e.target.value.toLowerCase();
 });
 $(".id").on("keyup", function(e) {
-    e.target.value = e.target.value.toLowerCase();
+	e.target.value = e.target.value.toLowerCase();
 });
 $(".d").on("keyup", function(e) {
-    e.target.value = e.target.value.replace(/[^\d]/, "");
+	e.target.value = e.target.value.replace(/[^\d]/, "");
 });
 
 function validateEmail(email) {
-    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(email);
 }
 var username, email, pass, ph, signup = 0;
 $('.circlebtn').click(function() {
-    if (signup == 0) {
-        username = $('.a').val();
-        email = $('.b').val();
-        pass = $('.c').val();
-        ph = $('.d').val();
-        if (username != "" && validateEmail(email) && pass != "" && ph.length == 10) {
-            signup = 1;
+	if (signup == 0) {
+		username = $('.a').val();
+		email = $('.b').val();
+		pass = $('.c').val();
+		ph = $('.d').val();
+		if (username != "" && validateEmail(email) && pass != "" && ph.length == 10) {
+			signup = 1;
 
-            $(".a").attr("disabled", "disabled");
-            $(".b").attr("disabled", "disabled");
-            $(".d").attr("disabled", "disabled");
-            $('.c').val("");
-            $('.c').focus();
+			$(".a").attr("disabled", "disabled");
+			$(".b").attr("disabled", "disabled");
+			$(".d").attr("disabled", "disabled");
+			$('.c').val("");
+			$('.c').focus();
 
-            $('.aa').text("Username");
-            $('.bb').text("Email");
-            $('.cc').text("Confirm Password");
-            $('.dd').text("Phone Number");
+			$('.aa').text("Username");
+			$('.bb').text("Email");
+			$('.cc').text("Confirm Password");
+			$('.dd').text("Phone Number");
 
-            $('.register-form h2').text("CONFIRM PASSWORD");
-            $('.close').css('display', 'none');
-            $('.register-form h2').css('color', '#ED2553');
-            $('.register-form').css({
-                "color": "#ED2553",
-                "background": "#FFF"
-            });
-            $('.register-form .form-group .form-label, .register-form .form-group .form-control').css('color', '#ED2553');
-        }
-    }
-    else if (signup == 1) {
-        if ($('.c').val() != pass) return;
+			$('.register-form h2').text("CONFIRM PASSWORD");
+			$('.close').css('display', 'none');
+			$('.register-form h2').css('color', '#ED2553');
+			$('.register-form').css({
+				"color": "#ED2553",
+				"background": "#FFF"
+			});
+			$('.register-form .form-group .form-label, .register-form .form-group .form-control').css('color', '#ED2553');
+		}
+	}
+	else if (signup == 1) {
+		if ($('.c').val() != pass) return;
 
-        const http = new XMLHttpRequest();
-        http.open('POST', '/signup');
-        http.setRequestHeader('Content-type', 'application/json');
-        http.onreadystatechange = function() {
-            if (http.readyState == XMLHttpRequest.DONE) {
-                if (http.responseText == 1) {
-                    $('.close').css({
-                        "display": "inline",
-                        "color": "#fff",
-                        "font-size": "25px",
-                        "transform": "rotate(0deg)",
-                        "line-height": "55px",
-                        "right": "15px"
-                    });
-                    $('.register-form').css('background', '#4BB543');
-                    $(".close").click();
-                    $('.close').text("✓");
-                    $(".close").off('click');
-                }
-                else if (http.responseText == 2) {
-                    swal("Error!", "A User With Same Email ID Already Exists!", "error")
-                        .then(() => { window.location.replace("/login"); });
-                }
-                else {
-                    swal("Error!", "Error While Creating This Account!", "error")
-                        .then(() => { window.location.replace("/login"); });
-                }
-            }
-        };
-        http.send(JSON.stringify({
-            username: username,
-            email: email,
-            pass: pass,
-            ph: ph
-        }));
-    }
+		const http = new XMLHttpRequest();
+		http.open('POST', '/signup');
+		http.setRequestHeader('Content-type', 'application/json');
+		http.onreadystatechange = function() {
+			if (http.readyState == XMLHttpRequest.DONE) {
+				if (http.responseText == 1) {
+					$('.close').css({
+						"display": "inline",
+						"color": "#fff",
+						"font-size": "25px",
+						"transform": "rotate(0deg)",
+						"line-height": "55px",
+						"right": "15px"
+					});
+					$('.register-form').css('background', '#4BB543');
+					$(".close").click();
+					$('.close').text("✓");
+					$(".close").off('click');
+				}
+				else if (http.responseText == 2) {
+					Swal.fire({
+						type: 'error',
+						title: 'Error!',
+						text: "A User With Same Email ID Already Exists!"
+					}).then(() => { window.location.replace("/login"); });
+				}
+				else {
+					Swal.fire({
+						type: 'error',
+						title: 'Error!',
+						text: "Error While Creating This Account!"
+					}).then(() => { window.location.replace("/login"); });
+				}
+			}
+		};
+		http.send(JSON.stringify({
+			username: username,
+			email: email,
+			pass: pass,
+			ph: ph
+		}));
+	}
 });
 
 
@@ -170,43 +188,43 @@ $('.circlebtn').click(function() {
 const m = 0.512286623256592433;
 
 function buildWave(w, h) {
-    const a = h / 4;
-    const y = h / 2;
-    const pathData = [
-        'M', w * 0, y + a / 2,
-        'c',
-        a * m, 0, -(1 - a) * m, -a,
-        a, -a,
-        's', -(1 - a) * m, a,
-        a, a,
-        's', -(1 - a) * m, -a,
-        a, -a,
-        's', -(1 - a) * m, a,
-        a, a,
-        's', -(1 - a) * m, -a,
-        a, -a,
+	const a = h / 4;
+	const y = h / 2;
+	const pathData = [
+		'M', w * 0, y + a / 2,
+		'c',
+		a * m, 0, -(1 - a) * m, -a,
+		a, -a,
+		's', -(1 - a) * m, a,
+		a, a,
+		's', -(1 - a) * m, -a,
+		a, -a,
+		's', -(1 - a) * m, a,
+		a, a,
+		's', -(1 - a) * m, -a,
+		a, -a,
 
-        's', -(1 - a) * m, a,
-        a, a,
-        's', -(1 - a) * m, -a,
-        a, -a,
-        's', -(1 - a) * m, a,
-        a, a,
-        's', -(1 - a) * m, -a,
-        a, -a,
-        's', -(1 - a) * m, a,
-        a, a,
-        's', -(1 - a) * m, -a,
-        a, -a,
-        's', -(1 - a) * m, a,
-        a, a,
-        's', -(1 - a) * m, -a,
-        a, -a,
-        's', -(1 - a) * m, a,
-        a, a,
-        's', -(1 - a) * m, -a,
-        a, -a
-    ].join(' ');
-    document.querySelector('#wave').setAttribute('d', pathData);
+		's', -(1 - a) * m, a,
+		a, a,
+		's', -(1 - a) * m, -a,
+		a, -a,
+		's', -(1 - a) * m, a,
+		a, a,
+		's', -(1 - a) * m, -a,
+		a, -a,
+		's', -(1 - a) * m, a,
+		a, a,
+		's', -(1 - a) * m, -a,
+		a, -a,
+		's', -(1 - a) * m, a,
+		a, a,
+		's', -(1 - a) * m, -a,
+		a, -a,
+		's', -(1 - a) * m, a,
+		a, a,
+		's', -(1 - a) * m, -a,
+		a, -a
+	].join(' ');
+	document.querySelector('#wave').setAttribute('d', pathData);
 }
 buildWave(90, 60);
