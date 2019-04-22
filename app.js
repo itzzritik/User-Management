5c690228c99c8fe1d22273e7d41c9b4c276989b2
Ritik Srivastava <ritik.space@gmail.com>
2019-04-22 20:31:57 +0530

    Attempting SQL Connection at every Query Request

--- app.js
+++ app.js
@@ -5,27 +5,6 @@ const clear = require('clear');
 const git = require('simple-git/promise')();
 const sql = require('mysql').createConnection(require("./sql"));
 
-connectionHandler();
-
-function connectionHandler() {
-    sql.connect((e) => {
-        if (e) {
-            console.log(">  Connection Failed \n•  " + e);
-            setTimeout(connectionHandler, 2000);
-        }
-        else { console.log(">  Connection Established"); }
-    });
-    sql.on('error', function(err) {
-        console.log("\n" + ++call + ") Connection to MySQL Server Failed");
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            console.log('   >  Error Code: ', err.code);
-            console.log('>  Attempting Reconnection');
-            connectionHandler();
-        }
-        else { throw err; }
-    });
-}
-
 var call = 0;
 app.set("view engine", "ejs");
 app.use('/public', express.static('public'));
@@ -70,24 +49,29 @@ app.post("/login", function(req, res) {
     var email = req.body.email,
         pass = req.body.pass;
     console.log("\n" + ++call + ") Authentication Started\n   >  Email: " + email);
-    sql.query("SELECT password FROM userData WHERE emailId = \"" + email + "\"", function(e, result) {
-        if (e) {
-            res.send("0");
-            console.log(">  Error occured while logging in :\n   >  " + e);
-        }
+    sql.connect((e) => {
+        if (e) console.log(">  Connection to MySQL Server Failed \n  >  " + e);
         else {
-            if (result.length == 0) {
-                res.send("2");
-                console.log(">  Authentication Terminated : User doesn't exist");
-            }
-            else if (result[0].password == pass) {
-                res.send("1");
-                console.log(">  Authentication Successfull");
-            }
-            else {
-                res.send("0");
-                console.log(">  Authentication Terminated : Invalid Password");
-            }
+            sql.query("SELECT password FROM userData WHERE emailId = \"" + email + "\"", function(e, result) {
+                if (e) {
+                    res.send("0");
+                    console.log(">  Error occured while logging in :\n   >  " + e);
+                }
+                else {
+                    if (result.length == 0) {
+                        res.send("2");
+                        console.log(">  Authentication Terminated : User doesn't exist");
+                    }
+                    else if (result[0].password == pass) {
+                        res.send("1");
+                        console.log(">  Authentication Successfull");
+                    }
+                    else {
+                        res.send("0");
+                        console.log(">  Authentication Terminated : Invalid Password");
+                    }
+                }
+            });
         }
     });
 });
@@ -100,28 +84,33 @@ app.post("/signup", function(req, res) {
         dateTime: new Date()
     };
     console.log("\n" + ++call + ") User Creation Started");
-    sql.query("SELECT password FROM userData WHERE emailId = \"" + userdata.emailId + "\"", function(e, result) {
-        if (e) {
-            res.send("0");
-            console.log(">  Error occured while logging in :\n   >  " + e);
-        }
+    sql.connect((e) => {
+        if (e) console.log(">  Connection to MySQL Server Failed \n  >  " + e);
         else {
-            if (result.length == 0) {
-                sql.query("INSERT INTO userData SET ?", userdata, function(e) {
-                    if (e) {
-                        res.send("0");
-                        console.log(">  Error While Creating Account\n   >  " + e);
+            sql.query("SELECT password FROM userData WHERE emailId = \"" + userdata.emailId + "\"", function(e, result) {
+                if (e) {
+                    res.send("0");
+                    console.log(">  Error occured while logging in :\n   >  " + e);
+                }
+                else {
+                    if (result.length == 0) {
+                        sql.query("INSERT INTO userData SET ?", userdata, function(e) {
+                            if (e) {
+                                res.send("0");
+                                console.log(">  Error While Creating Account\n   >  " + e);
+                            }
+                            else {
+                                res.send("1");
+                                console.log(userdata);
+                            }
+                        });
                     }
                     else {
-                        res.send("1");
-                        console.log(userdata);
+                        res.send("2");
+                        console.log(">  Account Creation Terminated : User Already Exists");
                     }
-                });
-            }
-            else {
-                res.send("2");
-                console.log(">  Account Creation Terminated : User Already Exists");
-            }
+                }
+            });
         }
     });
 });
@@ -130,18 +119,23 @@ app.post("/profile", function(req, res) {
     var email = req.body.email,
         pass = req.body.pass;
     console.log("\n" + ++call + ") Profile Details Requested\n   >  Email: " + email);
-    sql.query("SELECT * FROM userData WHERE emailId = \"" + email + "\"", function(e, result) {
-        if (e) {
-            res.send("0");
-            console.log(">  Error occured while fetching profile :\n   >  " + e);
-        }
+    sql.connect((e) => {
+        if (e) console.log(">  Connection to MySQL Server Failed \n  >  " + e);
         else {
-            res.render("index", {
-                login: 0,
-                email: result[0].emailId,
-                pass: pass,
-                username: result[0].userName,
-                ph: result[0].phoneNo
+            sql.query("SELECT * FROM userData WHERE emailId = \"" + email + "\"", function(e, result) {
+                if (e) {
+                    res.send("0");
+                    console.log(">  Error occured while fetching profile :\n   >  " + e);
+                }
+                else {
+                    res.render("index", {
+                        login: 0,
+                        email: result[0].emailId,
+                        pass: pass,
+                        username: result[0].userName,
+                        ph: result[0].phoneNo
+                    });
+                }
             });
         }
     });
@@ -151,38 +145,48 @@ app.post("/delete", function(req, res) {
     var email = req.body.email,
         pass = req.body.pass;
     console.log("\n" + ++call + ") Delete Account Requested\n   >  Email: " + email);
-    sql.query("SELECT password FROM userData WHERE emailId = \"" + email + "\"", function(e, result) {
-        if (e) {
-            res.send("0");
-            console.log(">  Error occured while logging in :\n   >  " + e);
-        }
+    sql.connect((e) => {
+        if (e) console.log(">  Connection to MySQL Server Failed \n  >  " + e);
         else {
-            if (result.length == 0) { res.send("0"); }
-            else if (result[0].password == pass) {
-                sql.query("DELETE FROM userData WHERE emailId = \"" + email + "\"", function(e, result) {
-                    if (e) {
-                        res.send("0");
-                        console.log(">  Error occured while Deleting account :\n   >  " + e);
+            sql.query("SELECT password FROM userData WHERE emailId = \"" + email + "\"", function(e, result) {
+                if (e) {
+                    res.send("0");
+                    console.log(">  Error occured while logging in :\n   >  " + e);
+                }
+                else {
+                    if (result.length == 0) { res.send("0"); }
+                    else if (result[0].password == pass) {
+                        sql.query("DELETE FROM userData WHERE emailId = \"" + email + "\"", function(e, result) {
+                            if (e) {
+                                res.send("0");
+                                console.log(">  Error occured while Deleting account :\n   >  " + e);
+                            }
+                            else {
+                                res.send("1");
+                                console.log(">  Account successfully deleted");
+                            }
+                        });
                     }
-                    else {
-                        res.send("1");
-                        console.log(">  Account successfully deleted");
-                    }
-                });
-            }
-            else { res.send("0"); }
+                    else { res.send("0"); }
+                }
+            });
         }
     });
 });
 
 app.post("/table", function(req, res) {
-    sql.query("SELECT * FROM userData ORDER BY dateTime", function(e, result) {
-        if (e) {
-            res.send("0");
-            console.log(">  Error occured while fetching table :\n   >  " + e);
-        }
+    sql.connect((e) => {
+        if (e) console.log(">  Connection to MySQL Server Failed \n  >  " + e);
         else {
-            res.json(result);
+            sql.query("SELECT * FROM userData ORDER BY dateTime", function(e, result) {
+                if (e) {
+                    res.send("0");
+                    console.log(">  Error occured while fetching table :\n   >  " + e);
+                }
+                else {
+                    res.json(result);
+                }
+            });
         }
     });
 });
@@ -203,5 +207,4 @@ app.listen(8080, function() {
     clear();
     console.log("\n" + ++call + ") Starting Server");
     console.log(">  Server is Listening");
-    console.log("\n" + ++call + ") Connection to MySQL Server");
 });
